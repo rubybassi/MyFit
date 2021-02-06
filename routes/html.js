6b06460c03ddee854ae800f5a819d8a5f3fbbbf1
Sandeep Bassi <thecodingfiles@gmail.com>
2021-02-06 21:37:31 +0000

    removed redundant route

--- routes/html.js
+++ routes/html.js
@@ -14,8 +14,4 @@ router.get('/exercise', (req,res) => {
     res.sendFile(path.join(__dirname, '../public/exercise.html'));
 });
 
-router.get('/exercise/:id', (req, res) => {
-    res.send('test');
-});
-
 module.exports = router;
