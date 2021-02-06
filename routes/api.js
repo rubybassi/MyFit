8259174479f8315be2cf4363ef1418c1063b0b10
Sandeep Bassi <thecodingfiles@gmail.com>
2021-02-06 21:35:39 +0000

    added db queries to api routes

--- routes/api.js
+++ routes/api.js
@@ -1,20 +1,55 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
+const { Workout } = require("../models");
 
-router.get('/workouts', (req, res) => {
-    // get last workout
+// get last workout
+router.get("/workouts", (req, res) => {
+  Workout.find({})
+    .then(results => {
+      console.log(`all workouts ${results}`);
+      res.send(results);
+    })
+    .catch(error => {
+      res.status(500).send(error);
+    });
 });
 
-router.post('/workouts/', (req, res) => {
-    // create new workout
-})
-
-router.put('/workouts/', (req, res) => {
-    // put add exercise
+// post new exercise
+router.post('/workouts', ({ body }, res) => {
+  console.log(`workout created ${body}`);
+  Workout.create(body)
+      .then(results => {
+        res.send(results);
+      })
+      .catch((error) => {
+        res.status(500).send(error);
+      });
 });
 
-router.get('/workouts/range', (req, res) => {
-    // get workouts in range
+// update new exercise
+router.put("/workouts/:id", ({ body, params }, res) => {
+  console.log(`updated workout data ${body}`)
+  //  If you set new: true, findOneAndUpdate() will instead give you the object after update was applied.
+  Workout.findByIdAndUpdate(params.id,{ $push: { exercises: body } },{ new: true })
+    .then(results => {
+      res.send(results);
+    })
+    .catch(error => {
+      res.status(500).send(error);
+    });
+});
+
+// get workouts in range - lookup aggregate query function to use here
+router.get("/workouts/range", (req, res) => {
+// $addFields outputs documents that contain all existing fields from the input documents and newly added fields.  
+Workout.aggregate([{ $addFields: { totalDuration: { $sum: "$exercises.duration" } } }])
+    .then(results => {
+     // console.log(`aggregated range ${results}`);
+      res.send(results);
+    })
+    .catch(error => {
+      res.status(500).send(error);
+    });
 });
 
 module.exports = router;
