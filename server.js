7fd7c4a748ceba9b7f7fd0a4fe9605b712f97551
Sandeep Bassi <thecodingfiles@gmail.com>
2021-02-07 01:30:05 +0000

    fixing port error on deployment

--- server.js
+++ server.js
@@ -19,8 +19,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/api', require('./routes/api'));
 app.use('/', require('./routes/html'));
 
-//const port = process.env.PORT || 3000;
-const port = 3000;
+const port = process.env.PORT || 3000;
+//const port = 3000;
 app.listen(port,() => {
  console.log(`listening on port: ${port}`);
 });
