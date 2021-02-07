a51be7f294c46fdde1f85ec95b34a38caa168b6e
Sandeep Bassi <thecodingfiles@gmail.com>
2021-02-07 00:11:08 +0000

    added mongoose connection to server.js file and updated package.json file after reinstalling

--- server.js
+++ server.js
@@ -1,11 +1,14 @@
 // package and file dependencies
 const express = require('express');
 const path = require('path');
+const mongoose = require("mongoose");
 require('dotenv').config();
-const port = process.env.PORT || 3000;
 
 // initialise server
 const app = express();
+
+// connect to database for deployment
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/workout', { useNewUrlParser: true, useUnifiedTopology: true });
 
 // middleware
 app.use(express.urlencoded({ extended: true}));
@@ -16,6 +19,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/api', require('./routes/api'));
 app.use('/', require('./routes/html'));
 
+//const port = process.env.PORT || 3000;
+const port = 3000;
 app.listen(port,() => {
  console.log(`listening on port: ${port}`);
 });
